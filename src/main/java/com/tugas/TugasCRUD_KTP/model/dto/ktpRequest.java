2f5eef032b6a2703bd11a75f1307f0e73fbb3ce3
anneisza <rizaanneisza@gmail.com>
2026-03-14 15:54:33 +0700

    Membuat Class ktpRequest.java di package model/dto

--- src/main/java/com/tugas/TugasCRUD_KTP/model/dto/ktpRequest.java
+++ src/main/java/com/tugas/TugasCRUD_KTP/model/dto/ktpRequest.java
@@ -1,4 +1,24 @@
 package com.tugas.TugasCRUD_KTP.model.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import java.time.LocalDate;
+
+@Data
 public class ktpRequest {
+
+    @NotBlank(message = "Nomor KTP tidak boleh kosong")
+    @Size(min = 16, max = 16, message = "Nomor KTP harus 16 digit")
+    private String nomorKtp;
+
+    @NotBlank(message = "Nama lengkap tidak boleh kosong")
+    private String namaLengkap;
+
+    private String alamat;
+
+    private LocalDate tanggalLahir;
+
+    @NotBlank(message = "Jenis kelamin harus dipilih")
+    private String jenisKelamin;
 }
