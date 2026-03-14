9b77a0e9a87e0ea2b8560404cc4118df2dcece0e
anneisza <rizaanneisza@gmail.com>
2026-03-14 15:01:01 +0700

    Menambahkan Depedensi Mapstruct

---
+++ src/main/java/com/tugas/TugasCRUD_KTP/TugasCrudKtpApplication.java
@@ -0,0 +1,13 @@
+package com.tugas.TugasCRUD_KTP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TugasCrudKtpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TugasCrudKtpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tugas/TugasCRUD_KTP/TugasCrudKtpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tugas.TugasCRUD_KTP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TugasCrudKtpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
